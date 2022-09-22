23ccacc6853671f344fdef2b9e1c6ad8dfc1fbfb
Zyrkos <pedrocastro1995@hotmail.com>
2022-09-22 16:20:50 +0100

    .

--- nav.js
+++ nav.js
@@ -1,4 +1,14 @@
-window.addEventListener("scroll", function(){
-    let header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+const toggleButton = document.getElementsByClassName('toggle-button') [0]
+const navbarLinks = document.getElementsByClassName('navbar-links') [0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+    
 })
+
+
+
+/*window.addEventListener("scroll", function(){
+    let header = document.querySelector(".navbar");
+    header.classList.toggle("sticky", window.scrollY > 0);
+}*/
